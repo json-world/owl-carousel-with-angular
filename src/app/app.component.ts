56f4932701841b4387be349b92f1cede4d85df58
json-world <demo.jsonworld@gmail.com>
2022-03-04 17:52:23 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'owl-carousel';
+
+   mySlideImages = ['../assets/images/image1.jpg','../assets/images/image2.jpeg','../assets/images/image3.jpg'];
+ myCarouselImages =['../assets/images/image1.jpg','../assets/images/image2.jpeg','../assets/images/image3.jpg'];
+
+  mySlideOptions={items: 1, dots: true, nav: true};
+  myCarouselOptions={items: 3, dots: true, nav: true};
+}
+
+
